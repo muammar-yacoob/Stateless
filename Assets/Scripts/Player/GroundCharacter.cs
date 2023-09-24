bc9b752924a481be78c8ced860ced4dd2bd1b55d
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2023-09-25 00:03:56 +0100

    Character grounded

---
+++ Assets/Scripts/Player/GroundCharacter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class GroundCharacter : MonoBehaviour
+    {
+        [SerializeField] private float raycastDistance = 1.0f; 
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private Vector3 characterGroundOffset;
+        
+        private readonly Vector3 rayOffset = Vector3.up * 0.3f;
+
+        private void Update()
+        {
+            if (Physics.Raycast(transform.position + rayOffset, Vector3.down, out RaycastHit hit, raycastDistance, groundLayer))
+            {
+                transform.position = hit.point + characterGroundOffset;
+            }
+        }
+    }
+}
