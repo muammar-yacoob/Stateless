91cf35eff4ad4e77135674751cddda5794c0178e
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2023-10-15 18:09:27 +0100

    Improved zombie damage with health bar and fx

--- Assets/Scripts/Zombies/ZombieDamage.cs
+++ Assets/Scripts/Zombies/ZombieDamage.cs
@@ -1,21 +1,48 @@
-﻿using SparkCore.Runtime.Core;
+﻿using Cysharp.Threading.Tasks;
+using SparkCore.Runtime.Core;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Stateless.Zombies
 {
+    [RequireComponent(typeof(Animator))]
     public class ZombieDamage : InjectableMonoBehaviour, IDamageable
     {
+        [SerializeField] private Image healthFill;
+        [SerializeField] private int health = 10;
+        [SerializeField] private ParticleSystem deathParticles;
+        [SerializeField] private ParticleSystem hitParticles;
+        
+        private static readonly int Hit = Animator.StringToHash("Hit");
+        private static readonly int Die1 = Animator.StringToHash("Die");
+        
         private Zombie zombie;
+        private Animator anim;
+
         protected override void Awake()
         {
             base.Awake();
             zombie = GetComponent<Zombie>();
+            anim = GetComponent<Animator>();
         }
 
         public void TakeDamage(int damageAmount)
         {
             Debug.Log($"Zombie took {damageAmount} damage");
+            health -= damageAmount;
+            healthFill.fillAmount = (float)health / 10f;
+            hitParticles.Play();
+            anim.SetTrigger(Hit);
+            
+            if (health <= 0) Die();
+        }
+
+        private async UniTask Die()
+        {
             zombie.IsDying = true;
+            anim.SetTrigger(Die1);
+            await UniTask.Delay(1000);
+            deathParticles.Play();
             Destroy(gameObject);
         }
     }
